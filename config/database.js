20968183d8a0add3eea71a3d6bb9981fbad7e59e
Anderson Ângelo <anderson_angelo1@hotmail.com>
2021-12-13 01:28:56 -0300

    first commit

---
+++ config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', '@cluster0.4dzzk.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'strapi_landing_page'),
+        username: env('DATABASE_USERNAME', 'usuario_landing_page_new'),
+        password: env('DATABASE_PASSWORD', 'eUzGiRBYtrR8_8i'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '62c1f48f-12ae-446a-bb56-b1d4c6df522b'
+};
